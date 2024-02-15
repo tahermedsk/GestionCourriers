12967d1866c06c7181716693d5201bc3d5f3d88d
22108 <22108@esp.mr>
2024-02-15 09:17:05 +0000

    empty project

---
+++ Backend/GestionCourriers/src/main/java/esp/irt/courriers/GestionCourriersApplication.java
@@ -0,0 +1,13 @@
+package esp.irt.courriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCourriersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCourriersApplication.class, args);
+	}
+
+}
---
+++ Backend/GestionCourriers/src/test/java/esp/irt/courriers/GestionCourriersApplicationTests.java
@@ -0,0 +1,13 @@
+package esp.irt.courriers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCourriersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
