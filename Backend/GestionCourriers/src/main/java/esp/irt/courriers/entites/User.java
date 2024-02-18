f6ae6529c02a21ccfb1b310d9c9f25a538f2120d
22034@esp.mr <abd22034>
2024-02-18 23:27:32 +0000

    SecurityConfig

---
+++ Backend/GestionCourriers/src/main/java/esp/irt/courriers/entites/User.java
@@ -0,0 +1,55 @@
+package esp.irt.courriers.entites;
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "users") // Assuming your table name is "users"
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private boolean enabled;
+
+    // Getters and setters (you can generate them using your IDE)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
