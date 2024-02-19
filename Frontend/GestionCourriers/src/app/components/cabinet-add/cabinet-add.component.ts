25efa35020b0ec3ab964cf7e755302b76438df30
22034@esp.mr <abd22034>
2024-02-19 08:43:00 +0000

    Models & services

---
+++ Frontend/GestionCourriers/src/app/components/cabinet-add/cabinet-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabinet-add',
+  templateUrl: './cabinet-add.component.html',
+  styleUrls: ['./cabinet-add.component.css']
+})
+export class CabinetAddComponent {
+
+}
---
+++ Frontend/GestionCourriers/src/app/models/courrier.spec.ts
@@ -0,0 +1,7 @@
+import { Courrier } from './courrier';
+
+describe('Courrier', () => {
+  it('should create an instance', () => {
+    expect(new Courrier()).toBeTruthy();
+  });
+});
