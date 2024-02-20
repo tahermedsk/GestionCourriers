e0110af43f7bcd358df06225e13a595eaa6e0deb
mourad365 <22095@esp.mr>
2024-02-20 17:48:36 +0000

    removing unuseful components

--- Frontend/GestionCourriers/src/app/app.module.ts
+++ Frontend/GestionCourriers/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
-import { CabinetListComponent } from './components/cabinet-list/cabinet-list.component';
-import { CabinetAddComponent } from './components/cabinet-add/cabinet-add.component';
-import { CabinetEditComponent } from './components/cabinet-edit/cabinet-edit.component';
 import { DestCourrierComponent } from './components/dest-courrier/dest-courrier.component';
 import { SendCourrierComponent } from './components/send-courrier/send-courrier.component';
 
@@ -16,9 +13,6 @@ import { SendCourrierComponent } from './components/send-courrier/send-courrier.
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    CabinetListComponent,
-    CabinetAddComponent,
-    CabinetEditComponent,
     DestCourrierComponent,
     SendCourrierComponent
   ],
