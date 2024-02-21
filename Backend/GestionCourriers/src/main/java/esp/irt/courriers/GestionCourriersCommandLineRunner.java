4de4e4463317faa8d9ae3135610dcc10ba083f1a
abdallahizerough <22034@esp.mr>
2024-02-21 20:27:04 +0000

    Création automatique d'administrateur

---
+++ Backend/GestionCourriers/src/main/java/esp/irt/courriers/GestionCourriersCommandLineRunner.java
@@ -0,0 +1,66 @@
+package esp.irt.courriers;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import esp.irt.courriers.security.model.Role;
+import esp.irt.courriers.security.model.UserEntity;
+import esp.irt.courriers.security.repository.RoleRepository;
+import esp.irt.courriers.security.repository.UserRepository;
+
+@Component
+public class GestionCourriersCommandLineRunner implements CommandLineRunner{
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public GestionCourriersCommandLineRunner(UserRepository userRepository,RoleRepository roleRepository,PasswordEncoder passwordEncoder){
+        super();
+        this.roleRepository=roleRepository;
+        this.userRepository=userRepository;
+        this.passwordEncoder=passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // TODO Auto-generated method stub
+        if(userRepository.count()==0 && roleRepository.count()==0) {
+
+                Role role=new Role("ADMIN");
+                Role role2=new Role("USERBD");
+                
+                roleRepository.save(role);
+                roleRepository.save(role2);
+            
+            
+                UserEntity userEntity=new UserEntity();
+                userEntity.setUsername("memine");
+                userEntity.setPassword(passwordEncoder.encode("36202464"));
+                
+                UserEntity userEntity2=new UserEntity();
+                userEntity2.setUsername("admin");
+                userEntity2.setPassword(passwordEncoder.encode("1234"));
+                Optional<Role> roles = roleRepository.findById(1L);
+
+	        
+	        if(roles.isPresent()) {
+	        userEntity.setRoles(Collections.singletonList(roles.get()));
+	        userEntity2.setRoles(Collections.singletonList(roles.get()));
+	        }
+
+            userRepository.save(userEntity);
+            userRepository.save(userEntity2);
+                
+
+        
+    }
+    
+}
+}
