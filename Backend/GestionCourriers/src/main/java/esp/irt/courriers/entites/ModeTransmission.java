b7be28946bccbd9101c9d6d55f948f8c3208d2c2
mourad365 <22095@esp.mr>
2024-03-08 00:10:43 +0000

    liaison de backend et frontend

--- Backend/GestionCourriers/src/main/java/esp/irt/courriers/entites/ModeTransmission.java
+++ Backend/GestionCourriers/src/main/java/esp/irt/courriers/entites/ModeTransmission.java
@@ -2,8 +2,9 @@ package esp.irt.courriers.entites;
 
 public enum ModeTransmission {
     mains_propres,
-    mail,
+    Mail,
     Fax,
-    autre,
-    
+    Autre,
+    Accuse_de_reception,  // Nouvelle valeur
+    Cahier_de_transmission  // Nouvelle valeur
 }
