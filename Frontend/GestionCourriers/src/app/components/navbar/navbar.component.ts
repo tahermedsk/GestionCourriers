cd86d1879848583c788bd0b6b9f69662fcff4900
mourad365 <22095@esp.mr>
2024-05-14 23:30:39 +0000

    alimentation d'entete depuis fichier text

--- Frontend/GestionCourriers/src/app/components/navbar/navbar.component.ts
+++ Frontend/GestionCourriers/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 import { AuthService } from 'src/app/services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  
+  ministere: string = ''; // Variable pour stocker le nom du ministère
+  nomarab: string = ''; // Variable pour stocker le nom en arabe du ministère
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(private router: Router, private authService: AuthService, private http: HttpClient) {
   }
-
-  Usernav: any = 'admin';
-  rolesUser: any;
 
   ngOnInit(): void {
+    // Charger les données du fichier texte lors de l'initialisation du composant
+    this.loadTextData();
   }
 
+  loadTextData() {
+    // Charger les données du fichier texte en utilisant HttpClient
+    this.http.get('assets/noms.txt', { responseType: 'text' })
+      .subscribe(data => {
+        // Diviser les données en lignes
+        const lines = data.split('\n');
+        // Parcourir les lignes et extraire les valeurs pour les variables appropriées
+        lines.forEach(line => {
+          const parts = line.split('=');
+          if (parts.length === 2) {
+            const key = parts[0].trim();
+            const value = parts[1].trim();
+            // Affecter les valeurs aux variables appropriées en fonction de la clé
+            if (key === 'ministere') {
+              this.ministere = value;
+            } else if (key === 'nomarab') {
+              this.nomarab = value;
+            }
+          }
+        });
+      });
+  }
 
   isLoginPage() {
     return this.router.url === '/login';
   }
-
 
   gotoResePassword() {
     this.logout();
@@ -32,5 +56,4 @@ export class NavbarComponent {
   logout() {
     this.authService.logout();
   }
-
 }
